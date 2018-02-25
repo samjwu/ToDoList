a6658cd5bbac82858400b7f4919594d70040252e
unknown <swm1000@hotmail.com>
2018-02-25 08:56:17 -0500

    items list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(function() {
-    
+    var todoitems =[
+        {
+            item: "Buy a new PC",
+            done: true
+        },
+        {
+            item: "Buy a new car",
+            done: false
+        },
+        {
+            item: "Buy a new house",
+            done: false
+        }
+    ];
 });
